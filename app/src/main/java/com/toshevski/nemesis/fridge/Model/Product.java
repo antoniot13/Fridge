aaf1457b754baeaf75ae31a5bfac0bd5b4f199f3
nemesis <toshevski@live.de>
2016-11-24 22:03:42 +0100

    fix

--- app/src/main/java/com/toshevski/nemesis/fridge/Model/Product.java
+++ app/src/main/java/com/toshevski/nemesis/fridge/Model/Product.java
@@ -1,7 +1,13 @@
 package com.toshevski.nemesis.fridge.Model;
 
-/**
- * Created by nemesis on 24-Nov-16.
- */
 public class Product {
+    public String Name;
+    public double Quantity;
+    public boolean IsAvailable;
+
+    public Product(String name, double quantity, boolean isAvailable) {
+        Name = name;
+        Quantity = quantity;
+        IsAvailable = isAvailable;
+    }
 }
