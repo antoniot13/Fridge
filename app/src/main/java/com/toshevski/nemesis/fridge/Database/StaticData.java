dae154840d426b446c080302dd068aea7c290ca9
nemesis <toshevski@live.de>
2016-11-24 21:59:14 +0100

    fix

---
+++ app/src/main/java/com/toshevski/nemesis/fridge/Database/StaticData.java
@@ -0,0 +1,18 @@
+package com.toshevski.nemesis.fridge.Database;
+
+import com.toshevski.nemesis.fridge.Model.Market;
+import com.toshevski.nemesis.fridge.Model.Product;
+
+import java.util.ArrayList;
+
+
+public class StaticData {
+
+    public static ArrayList<Market> getMarkets() {
+        return null;
+    }
+
+    public static ArrayList<Product> getProducts() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/toshevski/nemesis/fridge/Model/Market.java
@@ -0,0 +1,7 @@
+package com.toshevski.nemesis.fridge.Model;
+
+/**
+ * Created by nemesis on 24-Nov-16.
+ */
+public class Market {
+}
