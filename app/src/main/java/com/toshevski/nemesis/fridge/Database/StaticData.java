fc5989cce277a00e905fa6fcca938d0ba12caaff
Antonio Todorov <antonio.todorov13@gmail.com>
2016-11-27 13:59:05 +0100

    MyRecipesActivity without "itemClick" option

--- app/src/main/java/com/toshevski/nemesis/fridge/Database/StaticData.java
+++ app/src/main/java/com/toshevski/nemesis/fridge/Database/StaticData.java
@@ -4,6 +4,7 @@ import android.location.Location;
 
 import com.toshevski.nemesis.fridge.Model.Market;
 import com.toshevski.nemesis.fridge.Model.Product;
+import com.toshevski.nemesis.fridge.Model.Recipe;
 
 import java.util.ArrayList;
 
@@ -47,5 +48,41 @@ public class StaticData {
         products.add(new Product("Млеко", 1.0, true));
 
         return products;
+    }
+
+    public static ArrayList<Recipe> getRecipes() {
+        ArrayList<Product> products = new ArrayList<Product>();
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+        products.add(new Product("Јајца",2.0,false));
+        products.add(new Product("Млеко",0.5,false));
+        products.add(new Product("Шеќер",1.0,false));
+        products.add(new Product("Брашно",4.0,false));
+        products.add(new Product("Кисела вода",100.0,false));
+        String opis="Изматете ги јајцата, додајте им сол, шеќер и млеко. Додајте брашно и матете додека не ги стопите грутките кои се прават во тестото.\n" +
+                "\n" +
+                "Ова главно зависи од брашното, но треба да добиете многу густа смеса за палачинки.\n" +
+                "\n" +
+                "Додајте ја киселата вода и промешајте.\n" +
+                "\n" +
+                "Оставете ја смесата да отстои 20-ина минути, а потоа печете ги палачинките на силен оган";
+
+        recipes.add(new Recipe("Палачинки",products,opis));
+        products = new ArrayList<Product>();
+        products.add(new Product("Макарони",500.0,false));
+        products.add(new Product("Јајца",4.0,false));
+        products.add(new Product("Сирење бело",200.0,false));
+        products.add(new Product("Кисело млеко",800.0,false));
+        products.add(new Product("Млеко",100.0,false));
+        products.add(new Product("Сланина",150.0,false));
+        opis="1. Макароните со форма по избор (овде се спирални), се варат по упатството, се цедат и се ставаат во тава намастена со малку масло.\n" +
+                "2. Јајцата се матат, во нив се става дробено или рендано сирење, се меша и оваа смеса само се распоредува врз варените макарони, но не се измешува. Се става во загреана рерна на 200 степени околу 15 минути.\n" +
+                "3. Тавата се вади од рерната, сланината се сечка на коцкички и се распоредува одозгора врз макароните.\n" +
+                "4. Во киселото млеко се става прашокот за крем супа од печурки и млекото, се меша убаво и се истура рамномерно врз макароните и сланината. Се враќа во рерната и се допекува уште 15-20 минути.\n" ;
+
+        recipes.add(new Recipe("Макарони со сланина",products,opis));
+
+        return recipes;
+
+
     }
 }
