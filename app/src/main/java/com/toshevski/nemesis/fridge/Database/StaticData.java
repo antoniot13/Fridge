d46bd1ebe8896f14f1f4c93651bae0a760d7b5f3
Vlado Taseski <taseski.vlado@gmail.com>
2016-11-27 11:48:01 +0100

    fix markets
    fix markets

--- app/src/main/java/com/toshevski/nemesis/fridge/Database/StaticData.java
+++ app/src/main/java/com/toshevski/nemesis/fridge/Database/StaticData.java
@@ -11,8 +11,21 @@ import java.util.ArrayList;
 public class StaticData {
 
     public static ArrayList<Market> getMarkets() {
+
+        Location targetLocation = new Location("");//provider name is unecessary
+        targetLocation.setLatitude(0.0d);//your coords of course
+        targetLocation.setLongitude(0.0d);
+
         ArrayList<Market> markets = new ArrayList<Market>();
-        // markets.add(new Market("SP",);
+        markets.add(new Market("СП", targetLocation));
+        markets.add(new Market("Тинекс", targetLocation));
+        markets.add(new Market("Веро", targetLocation));
+        markets.add(new Market("Рамстор", targetLocation));
+        markets.add(new Market("КАМ", targetLocation));
+        markets.add(new Market("Гранап", targetLocation));
+        markets.add(new Market("Рептил", targetLocation));
+        markets.add(new Market("Макс Лукс", targetLocation));
+        markets.add(new Market("Пожаренка", targetLocation));
 
         return markets;
     }
@@ -21,14 +34,17 @@ public class StaticData {
         ArrayList<Product> products = new ArrayList<Product>();
         products.add(new Product("Кашкавал", 1.0, true));
         products.add(new Product("Салама", 2.0, true));
-        products.add(new Product("Феферони", 1.0, false));
+        products.add(new Product("Феферони", 0.0, false));
         products.add(new Product("Павлака", 3.1, true));
         products.add(new Product("Сирење", 1.0, true));
         products.add(new Product("Кечап", 2.0, true));
-        products.add(new Product("Мајонез", 1.0, false));
+        products.add(new Product("Мајонез", 0.0, false));
         products.add(new Product("Сланина", 3.1, true));
         products.add(new Product("Свински врат", 1.0, true));
         products.add(new Product("Печурки", 2.0, true));
+        products.add(new Product("Јогурт", 1.1, true));
+        products.add(new Product("Моцарела", 2.5, true));
+        products.add(new Product("Млеко", 1.0, true));
 
         return products;
     }
--- app/src/main/java/com/toshevski/nemesis/fridge/View/MarketActivity.java
+++ app/src/main/java/com/toshevski/nemesis/fridge/View/MarketActivity.java
@@ -1,16 +1,76 @@
 package com.toshevski.nemesis.fridge.View;
 
+
+import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.toshevski.nemesis.fridge.Database.StaticData;
+import com.toshevski.nemesis.fridge.Model.Market;
+import com.toshevski.nemesis.fridge.Model.Product;
 import com.toshevski.nemesis.fridge.R;
+
+import java.util.ArrayList;
 
 
 public class MarketActivity extends AppCompatActivity {
+
+    MarketAdapter ma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
+
+        //Implementation of adapter
+        ma = new MarketAdapter();
+        ListView marketsInListView = (ListView)findViewById(R.id.listMarkets);
+        marketsInListView.setAdapter(ma);
+        ma.notifyDataSetChanged();
+        marketsInListView.setAlpha(1);
+
+    }
+
+    public class MarketAdapter extends BaseAdapter {
+
+        ArrayList<Market> markets = StaticData.getMarkets();
+
+        @Override
+        public int getCount() {
+            return markets.size();
+        }
+
+        @Override
+        public Market getItem(int arg0) {
+            return markets.get(arg0);
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return arg0;
+        }
+
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            LayoutInflater inflater = (LayoutInflater) MarketActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            arg1 = inflater.inflate(R.layout.list_view, arg2, false);
+
+            TextView name = (TextView)arg1.findViewById(R.id.textView1);
+            TextView quan = (TextView)arg1.findViewById(R.id.textView2);
+
+            Market market = markets.get(arg0);
+
+            name.setText(market.Name);
+
+            return arg1;
+        }
     }
 
 
