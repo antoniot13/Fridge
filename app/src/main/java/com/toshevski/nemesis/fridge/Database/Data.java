4e170d3bc5c3625e00e199182fa80e45d3b81874
nemesis <toshevski@live.de>
2017-01-25 17:43:52 +0100

    test async

--- app/src/main/java/com/toshevski/nemesis/fridge/Database/Data.java
+++ app/src/main/java/com/toshevski/nemesis/fridge/Database/Data.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
 
+import com.toshevski.nemesis.fridge.Model.Fridge;
 import com.toshevski.nemesis.fridge.Model.Market;
 import com.toshevski.nemesis.fridge.Model.Product;
 import com.toshevski.nemesis.fridge.Model.Recipe;
@@ -29,6 +30,21 @@ public class Data {
         dbc.getWritableDatabase().insert(DB.Markets.TABLE_NAME, null, cv);
     }
 
+    public void deleteFromMarkets(Market m) {
+        SQLiteDatabase db = dbc.getReadableDatabase();
+        String query = "DELETE FROM " + DB.Markets.TABLE_NAME + " WHERE name = " + m.Name;
+
+        db.rawQuery(query, null);
+    }
+
+    public void insertIntoReceipts(Recipe r) {
+        ContentValues cv = new ContentValues();
+        cv.put(DB.Recipes.NAME, r.Name);
+        cv.put(DB.Recipes.DESC, r.Description);
+
+        dbc.getWritableDatabase().insert(DB.Recipes.TABLE_NAME, null, cv);
+    }
+
 
     public void insertIntoProducts(Product m) {
         ContentValues cv = new ContentValues();
@@ -37,6 +53,26 @@ public class Data {
         cv.put(DB.Products.QTY, m.Quantity);
 
         dbc.getWritableDatabase().insert(DB.Products.TABLE_NAME, null, cv);
+    }
+
+    public void deleteFromProducts(Product m) {
+        SQLiteDatabase db = dbc.getReadableDatabase();
+        String query = "DELETE FROM " + DB.Products.TABLE_NAME + " WHERE name = " + m.Name;
+
+        db.rawQuery(query, null);
+    }
+
+    public void insertIntoFridges(Fridge f) {
+        ContentValues cv = new ContentValues();
+        cv.put(DB.Products.NAME, f.Name);
+        dbc.getWritableDatabase().insert(DB.Fridge.TABLE_NAME, null, cv);
+    }
+
+    public void deleteFromFridges(Fridge m) {
+        SQLiteDatabase db = dbc.getReadableDatabase();
+        String query = "DELETE FROM " + DB.Fridge.TABLE_NAME + " WHERE name = " + m.Name;
+
+        db.rawQuery(query, null);
     }
 
     public ArrayList<Market> getAllMarkets() {
@@ -112,6 +148,31 @@ public class Data {
                 double qty = Double.parseDouble(c.getString(3));
 
                 //mark = new Recipe();
+
+                m.add(mark);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return m;
+    }
+
+    public ArrayList<Fridge> getAllFridges() {
+        ArrayList<Fridge> m = new ArrayList<>();
+
+        SQLiteDatabase db = dbc.getReadableDatabase();
+
+        String query = "SELECT  * FROM " + DB.Fridge.TABLE_NAME;
+
+        Cursor c = db.rawQuery(query, null);
+
+        Fridge mark = null;
+        if (c.moveToFirst()) {
+            do {
+                String name = c.getString(1);
+                mark = new Fridge();
+                mark.Name = name;
+
 
                 m.add(mark);
             } while (c.moveToNext());
---
+++ app/src/main/java/com/toshevski/nemesis/fridge/Model/Fridge.java
@@ -0,0 +1,9 @@
+package com.toshevski.nemesis.fridge.Model;
+
+/**
+ * Created by nemesis on 25-Jan-17.
+ */
+
+public class Fridge {
+    public String Name;
+}
